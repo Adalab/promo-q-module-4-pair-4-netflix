c175e8ab4006ea7e9d8be2a24bd9a6bcc0ac7f63
Yaiza Soria Felices <yaizafelices@gmail.com>
2022-09-05 17:13:33 +0200

    Change index.js so it is more clear to read

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
+// Fichero src/index.js
+
+// Importamos los dos módulos de NPM necesarios para trabajar
 const express = require('express');
 const cors = require('cors');
+
+// Creamos el servidor
 const server = express();
+
+
+// Configuramos el servidor
+server.use(cors());
+server.use(express.json());
+
+// Arrancamos el servidor en el puerto 3000
+const port = 4000;
+server.listen(port, () => {
+  console.log('listening' + port);
+});
+
+//Pedimos todas las peliculas
 const movies = require('./data/movies.json');
 server.get('/movies', (req, resp) => {
   const id = req.query.id;
   resp.json({ sucess: true, movies });
 });
 
-server.use(cors());
-server.use(express.json());
-const port = 4000;
-server.listen(port, () => {
-  console.log('listening' + port);
-});
+
+
+
